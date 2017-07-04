ad9c0645192b2f7e094555df13b109a2d2098389
Dmitry Shechtman <damnian@gmail.com>
2017-07-05 00:09:48 +0300

    Add solution

---
+++ src/Net.Chdk.Meta.Providers.CameraModel.Xml/CameraModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Net.Chdk.Meta.Providers.CameraModel.Xml
+{
+    sealed class CameraModel
+    {
+        public string[] Names { get; set; }
+        public string Platform { get; set; }
+    }
+}
---
+++ src/Net.Chdk.Meta.Providers.CameraModel.Xml/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Net.Chdk.Meta.Providers.CameraModel.Xml
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddXmlPlatformProvider(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection
+                .AddSingleton<IPlatformProvider, XmlPlatformProvider>();
+        }
+    }
+}
